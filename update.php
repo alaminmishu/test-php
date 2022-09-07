1d5026adf9611ae4610d14503521b4eb9343ede0
alaminmishu <aaamishucse@gmail.com>
2022-09-07 21:57:26 +0600

    remove line space

--- update.php
+++ update.php
@@ -1,13 +1,10 @@
 <?php
 include_once './config/db.php';
-
 $sql = "UPDATE persons SET District = 'Tangail' WHERE CustomerName = 'abc'";
-
 if (mysqli_query($conn, $sql)) {
     echo "Record updated successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-
 // mysqli_close($conn);
 // $conn->close();
